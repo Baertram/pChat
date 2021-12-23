b3870627a8cdce1b2dd5bf594749b791b9d284cf
Sean McNamara <smcnam@gmail.com>
2021-12-23 08:35:54 -0500

    First commit of CharCount addon.

--- pChat/i18n/en.lua
+++ pChat/i18n/en.lua
@@ -499,7 +499,13 @@ local strings = {
 	PCHAT_MENTIONS_WHOLEWORD_NAME = "Match your names as whole words only?",
 	PCHAT_MENTIONS_WHOLEWORD_TOOLTIP = "Whether or not to match your character names as whole words only. If you have a very short name in your character name, you may want to turn this on.",
 
-
+	-- Coorbin20211222
+	-- CharCount settings strings
+	PCHAT_CHARCOUNTH = "Chat Window Header",
+	PCHAT_CHARCOUNT_ENABLE_CHECKBOX_NAME = "Show Character Count",
+	PCHAT_CHARCOUNT_ENABLE_CHECKBOX_TOOLTIP = "Displays the current number of characters typed into the chat text box out of the max limit of 350. It will appear at the top center position of the chat window.",
+	PCHAT_CHARCOUNT_ZONE_POST_TRACKER_NAME = "Show Zone Post Tracker",
+	PCHAT_CHARCOUNT_ZONE_POST_TRACKER_TOOLTIP = "Displays the timestamp of when you've last posted in zone chat in the current zone. The time resets when you change zone. Useful for posting announcements to zone chat."
 
 }
 
